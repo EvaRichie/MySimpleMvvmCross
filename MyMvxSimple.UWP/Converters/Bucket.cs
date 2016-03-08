65476822fe4d9c3ea01ee20365b9de330d0b8f4a
richie <evarichie@outlook.com>
2016-03-08 17:22:21 +0800

    Add Uwp base value converter.

--- MyMvxSimple.UWP/Converters/Bucket.cs
+++ MyMvxSimple.UWP/Converters/Bucket.cs
@@ -5,10 +5,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
 
 namespace MyMvxSimple.UWP.Converters
 {
-    public class ManageBoolVisibilityConverter : MvxNativeValueConverter<BoolVisibilityConverter>
+    //Currently not working...LOl
+    //public class ManageBoolVisibilityConverter : MvxNativeValueConverter<BoolVisibilityConverter>
+    //{
+    //}
+
+    public class ManageBoolVisibilityConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool && bool.Parse(value.ToString()))
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WhenValueConverter : IValueConverter
+    {
+        public object When { get; set; }
+
+        public object Value { get; set; }
+
+        public object Others { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if(value.GetType() == When.GetType() && value.Equals(When))
+            {
+                return Value;
+            }
+            else
+            {
+                return Others;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
